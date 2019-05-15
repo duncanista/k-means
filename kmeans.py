8b134f1a6bd6ec91d57c32bc26ad837b1a23259a
duncanista <30836115+duncanista@users.noreply.github.com>
2019-05-15 00:38:16 -0500

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,10 +1,19 @@
-import random
+import numpy as np, matplotlib.pyplot as plt
 from operator import itemgetter
 from math import sqrt
+from copy import deepcopy
+from random import random
 
+# k means
+class k_means:
+    def __init__(self, k = 4, tolerance = 0.001, iterations = 100):
+        self.k = k
+        self.tolerance = tolerance
+        self.iterations = iterations
+
+# kd tree nodes
 class kd_node:
     def __init__(self, data, left=None, right=None, axis=None, dimension=None):
-        # inicializacion parametros constructor
         self.data = data
         self.left = left
         self.right = right
@@ -21,22 +30,66 @@ class kd_node:
 def kd_tree(point_list=None, dimensions=None, depth=0):
     if dimensions is None:
         dimensions = len(point_list[0])
-
     if not point_list:
         return None
 
-    axis = depth % dimensions
+    if dimensions != 2:
+        axis = depth % dimensions
 
-    point_list.sort(key=itemgetter(axis))
-    mid = len(point_list)//2
+        point_list.sort(key=itemgetter(axis))
+        mid = len(point_list)//2
 
-    coordinate = point_list[mid]
-    if len(point_list) > 1:
-        left = kd_tree(point_list[:mid], dimensions, depth + 1)
-        right = kd_tree(point_list[mid+1:], dimensions, depth + 1)
-        return kd_node(coordinate, left, right, axis, dimensions)
+        coordinate = point_list[mid]
+        if len(point_list) > 1:
+            left = kd_tree(point_list[:mid], dimensions, depth + 1)
+            right = kd_tree(point_list[mid+1:], dimensions, depth + 1)
+            return kd_node(coordinate, left, right, axis, dimensions)
+        else:
+            return kd_node(coordinate, None, None, axis, dimensions)
     else:
-        return kd_node(coordinate, None, None, axis, dimensions)
+        dt = np.dtype('float')
+        divided = []
+        for i in range(4):
+            start = i * (len(point_list) // 4)
+            end = len(point_list) // 4 * (i + 1)
+            number = random()
+            center = np.array([number, number])
+
+            division = np.array(point_list[start:end], dtype=dt) + center
+
+            divided.append(division)
+        divided = np.array(divided, dtype=dt)
+        data = np.concatenate(divided, axis=0)
+        clusters_k = 3
+        n = data.shape[0]
+        c = data.shape[1]
+
+        mean = np.mean(data, axis=0)
+        std = np.std(data, axis=0)
+        medoids = np.random.randn(clusters_k, c) * std + mean
+
+        medoids_old = np.zeros(medoids.shape)  # to store old centers
+        medoids_new = deepcopy(medoids)  # Store new centers
+        clusters = np.zeros(n)
+        distances = np.zeros((n, clusters_k))
+
+        error = np.linalg.norm(medoids_new - medoids_old)
+
+        # When, after an update, the estimate of that center stays the same, exit loop
+        while error != 0:
+            # Measure the distance to every center
+            for i in range(clusters_k):
+                distances[:, i] = np.linalg.norm(data - medoids[i], axis=1)
+            # Assign all training data to closest center
+            clusters = np.argmin(distances, axis=1)
+
+            centers_old = deepcopy(medoids_new)
+            # Calculate mean for every cluster and update the center
+            for i in range(clusters_k):
+                medoids_new[i] = np.mean(data[clusters == i], axis=0)
+            error = np.linalg.norm(medoids_new - centers_old)
+        plt.scatter(data[:, 0], data[:, 1], s=6, c="pink")
+        plt.scatter(medoids_new[:, 0], medoids_new[:, 1], marker='+', c="purple", s=150)
 
 def euclidean_distance(first, second):
     distance = 0
@@ -44,45 +97,50 @@ def euclidean_distance(first, second):
         distance += pow((first[i] - second[i]),2)
     return sqrt(distance)
 
-def build_graph(current_node, graph, posx, posy, width):
-    w = width / 2
+def build_kd_graph(current_node, graph, posX, posY, width):
+    w = width // 2
 
     if current_node.left and current_node.left.data is not None:
-        graph.add_node(current_node.left, pos=(posx - w, posy - 10),
-                       label=current_node.left.data)
+        graph.add_node(current_node.left, pos=(posX - w, posY - 10), label=current_node.left.data)
         graph.add_edge(current_node, current_node.left)
-        build_graph(current_node.left, graph, posx - w, posy - 10, w)
+        build_kd_graph(current_node.left, graph, posX - w, posY - 10, w)
 
     if current_node.right and current_node.right.data is not None:
-        graph.add_node(current_node.right, pos=(posx + w, posy - 10),
-                       label=current_node.right.data)  # current_node.right.data
+        graph.add_node(current_node.right, pos=(posX + w, posY - 10), label=current_node.right.data)
         graph.add_edge(current_node, current_node.right)
-        build_graph(current_node.right, graph, posx + w, posy - 10, w)
+        build_kd_graph(current_node.right, graph, posX + w, posY - 10, w)
 
-
-
-def show_graph(nodeTree):
+def show_graph(nodeTree, dimension):
     import networkx as nx
     import matplotlib.pyplot as plt
 
-    G = nx.Graph()
-    G.add_node(nodeTree, pos=(0, 0), label=nodeTree.data)
+    if(dimension != 2):
+        G = nx.Graph()
+        G.add_node(nodeTree, pos=(0, 0), label=nodeTree.data)
 
-    build_graph(nodeTree, G, 0, 0, 100)
-    # positions  = nx.drawing.nx_agraph.graphviz_layout(G, prog="dot")
-    colors = []
-    for node in G:
-        colors.append('pink')
 
-    print("Drawing...")
-    nx.draw(G, pos=nx.get_node_attributes(G, 'pos'),
-            labels=nx.get_node_attributes(G, 'label'),
-            with_labels=True, node_color = colors,
-            font_size=7, font_color='black'
-            )
-    print("...done drawing")
-    print("Showing...")
+        build_kd_graph(nodeTree, G, 0, 0, 100)
+        # positions  = nx.drawing.nx_agraph.graphviz_layout(G, prog="dot")
+        colors = []
+        for node in G:
+            colors.append('pink')
+        nx.draw(G, pos=nx.get_node_attributes(G, 'pos'),
+                labels=nx.get_node_attributes(G, 'label'),
+                with_labels=True, node_color = colors,
+                font_size=7, font_color='black'
+                )
+
     plt.show()
-    print("...done showing")
+
+def read_file():
+    file = open("input.txt", 'r').read().splitlines()
+    coordinates = []
+    n, k = [int(i) for i in file[0].split(" ")]
+    print(n,k)
+    for line in file[1:]:
+        tuple = [float(x) for x in line.strip("()").split(",")]
+        coordinates.append(tuple)
+    return n, k, coordinates
+
 
 
